3950c30dc4075724727d83004c603d6b9b3356d8
Jack Gaino <github@jackgaino.com>
2022-04-24 11:14:58 -0400

    Bump version: 1.1.2 → 1.2.0

--- md2cf/__init__.py
+++ md2cf/__init__.py
@@ -1,2 +1,2 @@
-__version__ = '1.1.2'
+__version__ = '1.2.0'
 __url__ = 'https://github.com/iamjackg/md2cf'
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='md2cf',
-    version='1.1.2',
+    version='1.2.0',
     packages=['md2cf'],
     url='https://github.com/iamjackg/md2cf',
     license='MIT',
