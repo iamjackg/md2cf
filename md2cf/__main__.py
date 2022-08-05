7c28d888b284840537c28b3873e49c513be26f77
Björn Skoglund <bjorn.skoglund@compass.com>
2022-08-05 15:14:16 -0400

    Remove logging, replacing with sparse print

--- md2cf/__main__.py
+++ md2cf/__main__.py
@@ -1,7 +1,6 @@
 import argparse
 import getpass
 import hashlib
-import logging
 import os
 import pprint
 import re
@@ -18,8 +17,6 @@ from md2cf.document import Page
 
 
 CONTENT_HASH_REGEX = re.compile(r"\[v([a-f0-9]{40})]$")
-
-logging.basicConfig(level=logging.INFO)
 
 
 def get_parser():
--- md2cf/ignored_files.py
+++ md2cf/ignored_files.py
@@ -1,19 +1,15 @@
 """
 Allow checking files for ignored status in gitignore files in the repo.
 """
-import logging
 from pathlib import Path
 from typing import List
 
 from gitignore_parser import parse_gitignore
 
-log = logging.getLogger("ignored_files")
-log.setLevel(logging.WARNING)
-
 
 class IgnoredFiles:
     """
-    Use .is_ignored() to check path p against all
+    Use .is_ignored(p) to check path p against all
     found gitignore files in the parent paths of the repo.
     """
     def __init__(self, start_path):
@@ -21,6 +17,10 @@ class IgnoredFiles:
         self.matchers = [parse_gitignore(str(g)) for g in gitignores]
 
     def is_ignored(self, p: Path):
+        """
+        :param p: The path to the file to check
+        :return: True if the filepath is matched in any .gitignore matchers.
+        """
         return any([m(str(p)) for m in self.matchers])
 
 
@@ -40,14 +40,11 @@ def _collect_gitignores(start_path: Path) -> List[Path]:
     if p.is_file():
         p = p.parent
     while str(p) != '/':
-        log.debug("Looking for .gitignore in %s", p)
         gitignore_file = p.joinpath('.gitignore')
         if gitignore_file.exists() and gitignore_file.is_file():
-            log.info("Found .gitignore %s", p)
             ret.append(gitignore_file)
         git_dir = p.joinpath('.git')
         if git_dir.exists() and git_dir.is_dir():
-            log.debug("Found %s, returning", git_dir)
             return ret
         p = p.parent
 
