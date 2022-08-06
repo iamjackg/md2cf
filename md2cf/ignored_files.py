a9d3cb3f14b6fc8fb5a1aa174b5b4b13297c5456
Björn Skoglund <bjorn.skoglund@compass.com>
2022-08-06 07:15:31 -0400

    Some polish from code review

--- md2cf/ignored_files.py
+++ md2cf/ignored_files.py
@@ -13,7 +13,9 @@ class GitRepository:
     of the tree. Note that there are cases where .git folders may exist
     in other parts of the tree. For example in terraform repositories
     the .terraform folder may contain copies of the tree including the .git
-    folder.
+    folder. This can be handled by initializing the GitRepository from a
+    known git root. After that all files will be correctly handled by the
+    is_ignored() method.
     """
     def __init__(self, repo_path: Path, use_gitignore=True):
         self.use_gitignore = use_gitignore
@@ -26,10 +28,11 @@ class GitRepository:
         :param start_path: A file or directory path to start searching from
         :return: The root directory of the git repo.
         """
+        fs_root = Path("/")
         p = start_path.absolute()
         if p.is_file():
             p = p.parent
-        while str(p) != '/':
+        while p != fs_root:
             git_dir = p.joinpath('.git')
             if git_dir.exists() and git_dir.is_dir():
                 return p
@@ -40,19 +43,19 @@ class GitRepository:
     def collect_gitignores(self, filepath: Path) -> List[Path]:
         """
         Collect all .gitignore files from start location to the root of the
-        repository. Note that a .git directory must be present in the parent tree
-        for the .gitignore files to be respected.
+        repository. Filepath is assumed to be a subdirectory of the git root.
+        If not, an error is printed a an empty list is returned.
 
         :param filepath: The path to start searching for .gitignore files
         :return: List of paths to .gitignore files relevant for start_path
         """
-
+        fs_root = Path("/")
         ret = list()
 
         p = filepath.absolute()
         if p.is_file():
             p = p.parent
-        while str(p) != '/':
+        while p != fs_root:
             gitignore_file = p.joinpath('.gitignore')
             if gitignore_file.exists() and gitignore_file.is_file():
                 ret.append(gitignore_file)
@@ -66,7 +69,9 @@ class GitRepository:
 
     def is_ignored(self, filepath: Path) -> bool:
         """
-        Check if filepath is ignored in the git repository
+        Check if filepath is ignored in the git repository by fetching all gitignores
+        in the tree down to the git root and checking all of them.
+
         :param filepath: Path to the file to check if it is ignored.
         :return: True if the file is ignored in any .gitignore file
         """
