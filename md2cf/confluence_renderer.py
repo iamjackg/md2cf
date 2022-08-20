6c029c4c13710037fd4e5b3a307c982f6363b332
Jack Gaino <github@jackgaino.com>
2022-08-19 20:51:41 -0400

    Remove leftover code, lint

--- md2cf/confluence_renderer.py
+++ md2cf/confluence_renderer.py
@@ -82,15 +82,6 @@ class ConfluenceRenderer(mistune.Renderer):
         body_tag.text = text
         return body_tag
 
-    def text(self, text):
-        """
-        Rendering unformatted text.
-        Replace newlines with spaces to create well-aligned paragraphs in Confluence.
-
-        :param text: text content.
-        """
-        return super().text(text.replace('\n',' '))
-
     def block_code(self, code, lang=None):
         root_element = self.structured_macro("code")
         if lang is not None:
