f61e8abc7727e0e4903e9affd34ab99cd23691e0
Jack Gaino <github@jackgaino.com>
2023-03-11 16:48:30 -0500

    Bump version: 2.0.1 → 2.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="md2cf",
-    version="2.0.1",
+    version="2.0.2",
     packages=["md2cf"],
     url="https://github.com/iamjackg/md2cf",
     license="MIT",
